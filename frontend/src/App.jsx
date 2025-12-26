f71d2b7c4f9fb6ec6c947e698a566c508d8043ff
Goyo Cancio <gcancio99@gmail.com>
2025-12-26 11:47:35 +0100

    New components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import ClerkProviderWithRoutes from "./auth/ClerkProviderWithRoutes.jsx";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout.jsx";
 import AuthenticationPage from "./auth/AuthenticationPage.jsx";
-import ChallengeGenerator from "./challenge/ChallengeGenerator.jsx";
+import { ChallengeGenerator } from "./challenge/ChallengeGenerator.jsx";
 import HistoryPanel from "./history/HistoryPanel.jsx";
 
 function App() {
